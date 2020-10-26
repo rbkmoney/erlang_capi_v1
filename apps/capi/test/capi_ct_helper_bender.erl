184af9c717a3123f06a102c61588e523aa01e8aa
Boris <bpozdnyakov@gmail.com>
2020-10-26 14:02:51 +0300

    Update lechiffre (#22)

--- apps/capi/test/capi_ct_helper_bender.erl
+++ apps/capi/test/capi_ct_helper_bender.erl
@@ -2,8 +2,15 @@
 
 -include_lib("bender_proto/include/bender_thrift.hrl").
 
+-type tid() :: any().
+-type internal_id() :: binary().
+-type msg_pack() :: msgpack_thrift:'Value'().
+
 -export([get_result/1]).
 -export([get_result/2]).
+-export([create_storage/0]).
+-export([del_storage/1]).
+-export([get_internal_id/3]).
 
 -spec get_result(binary()) -> bender_thrift:bender_GenerationResult().
 -spec get_result(binary(), msgpack_thrift:'Value'() | undefined) -> bender_thrift:bender_GenerationResult().
@@ -16,3 +23,27 @@ get_result(ID, Context) ->
         internal_id = ID,
         context = Context
     }.
+
+-spec create_storage() -> tid().
+-spec del_storage(tid()) -> ok.
+-spec get_internal_id(tid(), internal_id(), msg_pack()) -> bender_thrift:bender_GenerationResult().
+
+create_storage() ->
+    ets:new(bender_storage, [set, public]).
+
+del_storage(Tid) ->
+    ets:delete(Tid).
+
+get_internal_id(Tid, IdempotentKey, MsgPack) ->
+    case ets:lookup(Tid, IdempotentKey) of
+        [] ->
+            ets:insert(
+                Tid,
+                {IdempotentKey, #{
+                    ctx => MsgPack
+                }}
+            ),
+            {ok, get_result(IdempotentKey)};
+        [{IdempotentKey, #{ctx := Ctx}}] ->
+            {ok, get_result(IdempotentKey, Ctx)}
+    end.
